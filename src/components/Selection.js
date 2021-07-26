5f904dc8b811960f309922d744e3b11804c16889
Alec Dewulf <alecdewulf@Alecs-Air.hitronhub.home>
2021-07-26 10:46:59 -0400

    added initials to data

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -89,6 +89,7 @@ const Selection = ({ keyVal, keyPressed, setKeyVal, setDisplay,
 
         // data that will be saved in the realtime database
         const userData = {
+            initials: initials,
             blockReport: trial,
             condition: condition,
             cuestr: "",
